573b336147ae38622aa538b752862fdaf32ae75a
Simon <sstur@me.com>
2023-06-13 09:44:00 -0700

    Implement safeAsync

--- src/routes.ts
+++ src/routes.ts
@@ -9,48 +9,55 @@ import { createId } from './support/createId';
 import { sign } from './support/signature';
 import { imageByType, validateImageFileName } from './support/image';
 import { pipeStreamAsync } from './support/pipeStreamAsync';
+import { safeAsync } from './support/safeAsync';
 
 const UPLOADS_DIR = 'uploads';
 const uploadsDir = resolve(__dirname, '..', UPLOADS_DIR);
 
 export function attachRoutes(app: Application) {
-  app.get('/images/:fileName', async (request, response, next) => {
-    const fileName = request.params.fileName ?? '';
-    // Note: We don't verify the signature here because in cases where the
-    // signing key is not specified in the environment variable, it will change
-    // each time the server starts.
-    const imageDetails = validateImageFileName(fileName, {
-      verifySignature: false,
-    });
-    if (!imageDetails) {
-      return next();
-    }
-    const filePath = resolve(uploadsDir, fileName);
-    const stats = await fs.stat(filePath);
-    if (!stats.isFile()) {
-      return next();
-    }
-    response.header('Content-Type', imageDetails.type);
-    const readStream = createReadStream(filePath);
-    await pipeStreamAsync(readStream, response);
-  });
+  app.get(
+    '/images/:fileName',
+    safeAsync(async (request, response, next) => {
+      const fileName = request.params.fileName ?? '';
+      // Note: We don't verify the signature here because in cases where the
+      // signing key is not specified in the environment variable, it will change
+      // each time the server starts.
+      const imageDetails = validateImageFileName(fileName, {
+        verifySignature: false,
+      });
+      if (!imageDetails) {
+        return next();
+      }
+      const filePath = resolve(uploadsDir, fileName);
+      const stats = await fs.stat(filePath);
+      if (!stats.isFile()) {
+        return next();
+      }
+      response.header('Content-Type', imageDetails.type);
+      const readStream = createReadStream(filePath);
+      await pipeStreamAsync(readStream, response);
+    }),
+  );
 
-  app.post('/images', async (request, response) => {
-    const contentType =
-      request.header('content-disposition') ??
-      request.header('content-type') ??
-      '';
-    const imageType = imageByType.get(contentType);
-    if (!imageType) {
-      response.status(400).send({ error: 'Bad Request' });
-      return;
-    }
-    const id = sign(createTimestamp() + createId() + imageType.id);
-    await fs.mkdir(uploadsDir, { recursive: true });
-    const fileName = id + '.' + imageType.ext;
-    const filePath = resolve(uploadsDir, fileName);
-    const writeStream = createWriteStream(filePath);
-    await pipeStreamAsync(request, writeStream);
-    response.json({ url: `/images/${fileName}` });
-  });
+  app.post(
+    '/images',
+    safeAsync(async (request, response) => {
+      const contentType =
+        request.header('content-disposition') ??
+        request.header('content-type') ??
+        '';
+      const imageType = imageByType.get(contentType);
+      if (!imageType) {
+        response.status(400).send({ error: 'Bad Request' });
+        return;
+      }
+      const id = sign(createTimestamp() + createId() + imageType.id);
+      await fs.mkdir(uploadsDir, { recursive: true });
+      const fileName = id + '.' + imageType.ext;
+      const filePath = resolve(uploadsDir, fileName);
+      const writeStream = createWriteStream(filePath);
+      await pipeStreamAsync(request, writeStream);
+      response.json({ url: `/images/${fileName}` });
+    }),
+  );
 }
