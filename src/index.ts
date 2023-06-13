246bf3f49a201aeeecf4acd287fa299729f326f7
Simon <sstur@me.com>
2023-06-13 13:32:54 -0700

    Add 404 and 500 handlers to Express

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import './env';
 
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import { attachHandlers } from './server';
 
@@ -16,6 +16,22 @@ app.get('/', (request, response) => {
     `<p>Open to the <a href="/playground">GraphQL Playground</a></p>`,
   );
 });
+
+app.use((request, response) => {
+  response.status(404).json({ error: 'Not Found' });
+});
+
+app.use(
+  (err: unknown, request: Request, response: Response, _next: NextFunction) => {
+    const error = err instanceof Error ? err : new Error(String(err));
+    // eslint-disable-next-line no-console
+    console.error(error);
+    const status: unknown = Object(err).status;
+    response
+      .status(typeof status === 'number' ? status : 500)
+      .json({ error: String(error) });
+  },
+);
 
 app.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`);
